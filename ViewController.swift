1f9f791014dbc798f21eb8e46c0fd4131738b34b
Paul Tangen <ptangen@ptangen.com>
2016-09-28 02:04:53 -0400

    Complete the IBOutlet and IBActions readme.

--- ViewController.swift
+++ ViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textDisplay: UILabel!
 
+    @IBAction func HelloWorld(_ sender: UIButton) {
+        textDisplay.text = "Hello, world!"
+        print("Pressed Hello World button")
+    }
+    
+    @IBAction func GoodBye(_ sender: UIButton) {
+        textDisplay.text = "Good bye, world!"
+        print("Pressed Good bye World button")
+    }
+    
+    @IBAction func unicorn(_ sender: AnyObject) {
+        print("Pressed Unicorn! button")
+        textDisplay.text = "x"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        textDisplay.text = "Zzz..."
     }
 
     override func didReceiveMemoryWarning() {
